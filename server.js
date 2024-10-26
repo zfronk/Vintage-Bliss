db997ce56cffdd3cc6f876b7afb2f8f348cb61a8
Steve Aboll <officialstevve@gmail.com>
2024-10-26 12:25:28 +0300

    Ready

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/' , function(req , res){
+  res.sendFile(path.join(__dirname, 'vintage.html'));
+});
+
+
+app.listen(PORT , function(){
+
+  console.log(`Server is running on http://localHost:${PORT}`);
+})
+
+
